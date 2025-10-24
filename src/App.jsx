ef68c87ac075de4e1eb34601cd8cafcc235555be
Nikhil Sanam <nikhil.sanam25@gmail.com>
2025-10-24 06:38:46 +0530

    navbar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // enables navbar toggle
 import Home from "./Home";
 import Doctors from "./Doctors";
 import Register from "./Register";
@@ -9,20 +10,47 @@ import Login from "./Login";
 function App() {
   return (
     <Router>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      {/* Navbar */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-success">
         <div className="container">
-          <Link className="navbar-brand" to="/">Hospital Management</Link>
-          <div>
+          <Link className="navbar-brand fw-bold" to="/">
+            Hospital Management
+          </Link>
+
+          {/* Mobile menu toggle button */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Collapsible navigation links */}
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto">
-              <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/doctors">Doctors</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/register">Register</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/login">Login</Link></li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/doctors">Doctors</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">Register</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+              </li>
             </ul>
           </div>
         </div>
       </nav>
 
+      {/* Page Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctors />} />
